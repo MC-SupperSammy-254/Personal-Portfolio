1479fd0582a09ed83d25e2b003161f7e9ab130fd
Jackline-Jebet <jebetjackline2016@gmail.com>
2023-09-05 19:08:40 +0300

    updating

--- script.js
+++ script.js
@@ -41,3 +41,51 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   });
 });
+
+
+// function scrollGallery(selector, value) {
+//     document.querySelector(selector).scrollBy({
+//         left: value,
+//         behavior: 'smooth'
+//     });
+// }
+
+// document.getElementById("prevBtn").addEventListener("click", function() {
+//     scrollGallery(".photos", -200);  // For photos
+//     scrollGallery(".videos", -330);  // For videos (320 + 10 margin)
+// });
+
+// document.getElementById("nextBtn").addEventListener("click", function() {
+//     scrollGallery(".photos", 200);   // For photos
+//     scrollGallery(".videos", 330);   // For videos (320 + 10 margin)
+// });
+
+
+
+document.getElementById("prevBtn").addEventListener("click", function() {
+    document.querySelector(".photos").scrollBy({
+        left: -200, // This value can be adjusted based on how much you want to scroll
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById("nextBtn").addEventListener("click", function() {
+    document.querySelector(".photos").scrollBy({
+        left: 200, // This value can be adjusted based on how much you want to scroll
+        behavior: 'smooth'
+    });
+ });
+
+ document.getElementById("videoPrevBtn").addEventListener("click", function() {
+    document.querySelector(".videos").scrollBy({
+        left: -330, // Adjust as needed (320 + 10 margin)
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById("videoNextBtn").addEventListener("click", function() {
+    document.querySelector(".videos").scrollBy({
+        left: 330, // Adjust as needed (320 + 10 margin)
+        behavior: 'smooth'
+    });
+});
